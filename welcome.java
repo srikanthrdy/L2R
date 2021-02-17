3c3f809b31b46cad8622800d371eeffefe229560
Gitea <gitea@fake.local>
2021-02-17 13:24:06 +0530

    modified & pushed

--- welcome.java
+++ welcome.java
@@ -3,8 +3,9 @@ public static void main(String[] args){
 System.out.println("welcome to Devops Certification program");
 System.out.println("###Intial code modified ###");
   
-  System.out.println("###The code is entered at github ###");
-  
+ System.out.println("###The code is entered at github ###");
+  System.out.println("###The code is entered after clone and pushed back to github ###");
+
 }
 }
 
