a543b6cdd8317de59534368387ca6994f2a6adb4
srikanthrdy <46310236+srikanthrdy@users.noreply.github.com>
2021-02-17 13:17:28 +0530

    Update welcome.java
    update at github

--- welcome.java
+++ welcome.java
@@ -2,6 +2,9 @@ class Welcome{
 public static void main(String[] args){
 System.out.println("welcome to Devops Certification program");
 System.out.println("###Intial code modified ###");
+  
+  System.out.println("###The code is entered at github ###");
+  
 }
 }
 
